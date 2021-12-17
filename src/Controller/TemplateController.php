b87985bf527632dea00d808343376412e35f2dbc
Lautaro Oleastro <lauchaoleastro1@gmail.com>
2021-12-17 11:30:36 -0300

    se agrego la pantalla de listado de templates

--- src/Controller/TemplateController.php
+++ src/Controller/TemplateController.php
@@ -2,20 +2,33 @@
 
 namespace App\Controller;
 
+use App\Repository\TemplateRepository;
+use App\Entity\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * @Route("/template")
+ */
 class TemplateController extends AbstractController
 {
     /**
-     * @Route("/template", name="template")
+     * @Route("/", name="templates", methods={"GET"})
      */
-    public function index(): Response
+    public function Index(TemplateRepository $templateRepository): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/TemplateController.php',
+        return $this->render('app/private/template/index.html.twig',[
+            'templates' => $templateRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/new", name="createTemplate")
+     */
+    public function Create(): Response
+    {
+        return $this->render('app/private/template/new.html.twig');
     }
 }
