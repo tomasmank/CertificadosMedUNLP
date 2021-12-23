9ddefd927f806f703f2982d57d41f79ce0695a5c
Lautaro Oleastro <lauchaoleastro1@gmail.com>
2021-12-22 23:42:49 -0300

    test 1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Attendee;
+use App\Entity\Event;
 
 /**
  * @Route("/")
@@ -22,9 +25,26 @@ class HomeController extends AbstractController
     /**
      * @Route("/certificados", name="public")
      */
-    public function certificados(): Response
+    public function certificados(Request $request): Response
     {
-        return $this->render('app/public/index.html.twig');
+        
+        $dni = null;
+        $attendee = null;
+        $events = null;
+
+        if ($request->request->has('_dni')) {
+            $dni = $request->request->get('_dni');
+            $attendee = $this->getDoctrine()
+                ->getRepository(Attendee::class)
+                ->findAttendeeByDni($dni);
+            if ($attendee) {
+                $events = $this->getDoctrine()
+                    ->getRepository(Event::class)
+                    ->getAllAttendedBy(1);
+            }
+        }
+
+        return $this->render('app/public/index.html.twig', [ 'dni' => $dni , 'attendee' => $attendee , 'events' => $events ]);
     }
 
   #  /**
--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -26,10 +26,24 @@ class EventRepository extends ServiceEntityRepository
             ->andWhere('e.name = :val')
             ->setParameter('val', $value)
             ->getQuery()
-            ->getResults()
+            ->getResult()
         ;
     }
     
+    public function getAllAttendedBy($attendee_id){
+
+        # $event = $this->findByName('Curso de anatomia');
+
+        $rsm = new ResultSetMapping();
+// build rsm here
+
+$query = $entityManager->createNativeQuery('SELECT id, name, discr FROM users WHERE name = ?', $rsm);
+$query->setParameter(1, 'romanb');
+
+$users = $query->getResult();
+
+        return $event;
+    }
 
      /**
       * @return Event[] Returns an array of Event objects
