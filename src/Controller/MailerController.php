ca04f1147a4be2eadb11ca01e7a8f9b7949fbf81
Lautaro Oleastro <lauchaoleastro1@gmail.com>
2021-12-28 18:48:01 -0300

    generar PDF. TODO: arreglar en Win

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -8,7 +8,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+
 use Knp\Snappy\Pdf;
+use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
 
 use App\Repository\AttendeeRepository;
 use App\Entity\Attendee;
@@ -27,15 +29,25 @@ class MailerController extends AbstractController
         ]);
     }
 
-    public function indexAction(Pdf $snappy)
+    public function pdfAction(Attendee $attendee, Event $event)
     {
-        $html = $this->renderView('index.html.twig');
+        $knpSnappyPdf = new Pdf('/usr/bin/wkhtmltopdf');
 
-        return $snappy->getOutputFromHtml($html);
+        $html = $this->renderView('app/public/certificate.html.twig', [
+            'attendee'  => $attendee,
+            'event' => $event
+        ]);
+
+        $filename = $attendee->getLastName() . $attendee->getFirstName() . '-' . $event->getName() . '.pdf';
+
+        return new PdfResponse(
+            $knpSnappyPdf->getOutputFromHtml($html),
+            $filename
+        );
     }
     
     /**
-     * @Route("/email/{attendeeID}/{eventID}", name="sendEmail")
+     * @Route("/certificados/{attendeeID}/{eventID}", name="sendEmail")
      */
     public function sendEmail(Request $request, MailerInterface $mailer, string $attendeeID, int $eventID)
     {
@@ -47,14 +59,15 @@ class MailerController extends AbstractController
             ->getRepository(Event::class)
             ->find($eventID);
 
-        # $snappy = new Pdf('/usr/bin/wkhtmltopdf');
-        # $pdfFile = $this->indexAction($snappy);
+        $pdfFile = $this->pdfAction($attendee, $event);
+        
         $email = (new Email())
             ->from('lauchaoleastro3@gmail.com')
             ->to($attendee->getEmail())
             ->subject('Certificado - ' . $event->getName())
             ->text('Este mensaje ha sido generado automáticamente. Por favor, no responder.');
             
+
         # $mailer->send($email);
 
         $this->addFlash("success", "Certificado enviado correctamente al mail: " . $attendee->getEmail() );
